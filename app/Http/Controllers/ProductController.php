58f827822fa12577dc9fc486d4c2ef7921f9ff17
enderson <enderson.sys@gmail.com>
2022-09-28 16:04:24 -0300

    Seeder and products dashboard

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function show($id) {
+        $product = Product::find($id);
+        
+        return view('products.show', [
+            'product' => $product
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminProductController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/{id}', [ProductController::class, 'show']);
+
+//Admin
+Route::prefix('admin/products')->name('admin.products.')->group(function() {
+    Route::get('', [AdminProductController::class, 'index'])->name('index');
+    Route::get('edit', [AdminProductController::class, 'edit'])->name('edit');
 });
